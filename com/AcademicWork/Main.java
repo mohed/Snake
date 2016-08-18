2932f235d6f99e56a6cd2cd5829dfad6dbeb5ac2
mohed <mohed@hotmail.com>
2016-08-18 23:02:09 +0200

    Minor cleanup

--- com/AcademicWork/Main.java
+++ com/AcademicWork/Main.java
@@ -3,7 +3,6 @@ package com.AcademicWork;
 import com.googlecode.lanterna.*;
 import com.googlecode.lanterna.input.Key;
 import com.googlecode.lanterna.terminal.Terminal;
-import com.googlecode.lanterna.terminal.TerminalSize;
 
 import java.nio.charset.Charset;
 import java.util.Random;
